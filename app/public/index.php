b9eeee4d933087da009c80aeef2df2f089c8ea68
Harry Webster <harry@diffordsguide.com>
2022-08-27 21:56:46 +0100

    Using new localhost domain

--- app/public/index.php
+++ app/public/index.php
@@ -2,12 +2,11 @@
 
 // function to discover current branch
 function get_branch() {
-	$domain = '.development.thirstytuesday.com';
+	$domain = '.sample.testcode.work';
 	$host = getenv('HTTP_HOST');
 	$branch = preg_replace('#'.$domain.'$#', '', $host);
 	return $branch;
 }
 
-echo "<pre>";
-echo "Hello world! - I am branch ".get_branch();
-echo "</pre>";
+echo "<h1>Hellow world!</h1>";
+echo "<p>I am branch <strong>".get_branch()."</strong></p>";
