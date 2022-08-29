7d969481362f26eb56e5b127080af25cff17a2ef
Harry Webster <me@harrywebster.com>
2022-08-29 13:00:27 +0100

    Fixing broken file

--- bin/entry.sh
+++ bin/entry.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# For debugging uncomment the following lines:
 apachectl start > /dev/null 2>&1
 /bin/bash
-
-# normal operations run Apache2 in foreground.
-#/usr/sbin/apache2 start
